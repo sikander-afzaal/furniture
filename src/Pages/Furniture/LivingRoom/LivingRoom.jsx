773d44d39bd00a54f1914fa37fb7565862dd6ef1
sikander-afzaal <72333576+sikander-afzaal@users.noreply.github.com>
2022-04-08 18:29:26 +0500

    first commit

--- src/Pages/Furniture/LivingRoom/LivingRoom.jsx
+++ src/Pages/Furniture/LivingRoom/LivingRoom.jsx
@@ -19,6 +19,23 @@ import popular6 from "../../../Assets/Living Room/Popular/6.png";
 import popular7 from "../../../Assets/Living Room/Popular/7.png";
 import popular8 from "../../../Assets/Living Room/Popular/8.png";
 import Card from "../../../Components/Card/Card.jsx";
+import registerBg from "../../../Assets/Living Room/register-bg.png";
+// beds---------------------------
+import bed1 from "../../../Assets/Living Room/beds/bed1.png";
+import bed2 from "../../../Assets/Living Room/beds/bed2.png";
+import bed3 from "../../../Assets/Living Room/beds/bed3.png";
+import bed4 from "../../../Assets/Living Room/beds/bed4.png";
+// small tables---------------------------
+import table1 from "../../../Assets/Living Room/small tables/table1.png";
+import table2 from "../../../Assets/Living Room/small tables/table2.png";
+import table3 from "../../../Assets/Living Room/small tables/table3.png";
+//popular(week)
+import imgPop1 from "../../../Assets/Living Room/week-popular/left1.png";
+import imgPop2 from "../../../Assets/Living Room/week-popular/left2.png";
+import imgPop3 from "../../../Assets/Living Room/week-popular/left3.png";
+import mainPop from "../../../Assets/Living Room/week-popular/main.png";
+import { faStar, faCartPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 function LivingRoom() {
   return (
     <div className="living-room">
@@ -94,6 +111,77 @@ function LivingRoom() {
           <Card img={popular2} desc={"Windsor Chair"} />
           <Card img={popular3} desc={"Egg Chair"} />
           <Card img={popular4} desc={"Wishbone Chair"} />
+        </div>
+      </div>
+      <div className="living-section4">
+        <img src={registerBg} alt="" />
+        <div className="abs-living-section4">
+          <h1>SUMMER CONFORT COLLECTION</h1>
+          <div className="input-div-living">
+            <h3>GET YOURSELF REGISTER</h3>
+            <div className="row-input">
+              <input type="text" placeholder="First Name" />
+              <input type="text" placeholder="Second Name" />
+            </div>
+            <input className="email-input" type="email" placeholder="Email" />
+            <button className="explore">SUBSCRIBE</button>
+          </div>
+        </div>
+      </div>
+      <div className="living-section3 padding-top">
+        <div className="cards-grid">
+          <Card img={bed1} desc={"Best High-End Sofa Brand: Maiden Home"} />
+          <Card img={bed2} desc={"Best High-End Sofa Brand: Maiden Home"} />
+          <Card img={bed3} desc={"Best High-End Sofa Brand: Maiden Home"} />
+          <Card img={bed4} desc={"Best High-End Sofa Brand: Maiden Home"} />
+        </div>
+      </div>
+      <div className="homepage-section5">
+        <div className="heading-section5">
+          <span className="line"></span>
+          <h1>Popular This Week</h1>
+        </div>
+        <div className="bottom-section5">
+          <div className="left-section5">
+            <div className="col-images">
+              <img src={imgPop1} alt="" />
+              <img src={imgPop2} alt="" />
+              <img src={imgPop3} alt="" />
+            </div>
+            <img src={mainPop} alt="" className="main-section5" />
+          </div>
+          <div className="right-section5">
+            <h2>Bunk Sofa Stainless Steel</h2>
+            <div className="combine">
+              <div className="star-div">
+                <FontAwesomeIcon icon={faStar} className="yellow" />
+                <FontAwesomeIcon icon={faStar} className="yellow" />
+                <FontAwesomeIcon icon={faStar} className="yellow" />
+                <FontAwesomeIcon icon={faStar} className="yellow" />
+                <FontAwesomeIcon icon={faStar} />
+              </div>
+              <h3>$299</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.{" "}
+              </p>
+            </div>
+
+            <button className="explore">
+              Add to Cart <FontAwesomeIcon icon={faCartPlus} />
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="living-section3 padding-top">
+        <div className="cards-grid">
+          <Card img={table1} desc={"Best High-End Sofa Brand: Maiden Home"} />
+          <Card img={table2} desc={"Best High-End Sofa Brand: Maiden Home"} />
+          <Card img={table3} desc={"Best High-End Sofa Brand: Maiden Home"} />
+          <Card img={table3} desc={"Best High-End Sofa Brand: Maiden Home"} />
         </div>
       </div>
     </div>
