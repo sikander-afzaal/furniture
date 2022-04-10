843e129f4a0429a22f191a64b4ada183f87b3aff
sikander-afzaal <72333576+sikander-afzaal@users.noreply.github.com>
2022-04-10 20:10:14 +0500

    first commit

--- src/Pages/Furniture/Workspace/WorkSpace.jsx
+++ src/Pages/Furniture/Workspace/WorkSpace.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import "./WorkSpace.css";
-import slide1 from "../../../Assets/WorkSpace/slide1.png";
+import slide1 from "../../../Assets/WorkSpace/slider/slide1.png";
+import slide2 from "../../../Assets/WorkSpace/slider/slide2.png";
 import { Splide, SplideSlide, SplideTrack } from "@splidejs/react-splide";
 import "@splidejs/react-splide/css";
 import {
   faLongArrowRight,
   faLongArrowLeft,
 } from "@fortawesome/free-solid-svg-icons";
+import new1 from "../../../Assets/WorkSpace/new/new1.png";
+import new2 from "../../../Assets/WorkSpace/new/new2.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 function WorkSpace() {
   return (
@@ -24,6 +27,13 @@ function WorkSpace() {
             autoplay: true,
             perPage: 1,
             perMove: 1,
+            type: "loop",
+            breakpoints: {
+              1130: {
+                width: "100%",
+                autoWidth: false,
+              },
+            },
           }}
         >
           <SplideTrack>
@@ -34,17 +44,7 @@ function WorkSpace() {
             </SplideSlide>
             <SplideSlide>
               <div className="work-img-wrapper">
-                <img src={slide1} alt="" />
-              </div>
-            </SplideSlide>
-            <SplideSlide>
-              <div className="work-img-wrapper">
-                <img src={slide1} alt="" />
-              </div>
-            </SplideSlide>
-            <SplideSlide>
-              <div className="work-img-wrapper">
-                <img src={slide1} alt="" />
+                <img src={slide2} alt="" />
               </div>
             </SplideSlide>
           </SplideTrack>
@@ -58,6 +58,36 @@ function WorkSpace() {
             </button>
           </div>
         </Splide>
+        <div className="abs-work">
+          <h1>Luxury Workspace</h1>
+          <div className="line work-line"></div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
+          </p>
+          <button className="explore inverted">
+            Shop <FontAwesomeIcon icon={faLongArrowRight} />
+          </button>
+        </div>
+      </div>
+      <div className="dining-section2 margin-top">
+        <div className="text-part">
+          <div className="heading-section5">
+            <span className="line"></span>
+            <h1>New Arrivals</h1>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
+          </p>
+          <button className="explore">
+            View More <FontAwesomeIcon icon={faLongArrowRight} />
+          </button>
+        </div>
+        <div className="row-img">
+          <img src={new1} alt="" />
+          <img src={new2} alt="" />
+        </div>
       </div>
     </div>
   );
